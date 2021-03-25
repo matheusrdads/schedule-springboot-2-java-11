aaadd6b6f87c18bf583291feaa05f9a6bc392b94
unknown <malbuquerquedesouza@gmail.com>
2021-03-25 17:22:32 -0300

    inicio do projeto

---
+++ src/test/java/com/scheduleapp/scheduleapp/ScheduleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleapp.scheduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
