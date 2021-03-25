904bbc0ed7c9a21bb3a0a2f0ba963d65afa6eda3
unknown <malbuquerquedesouza@gmail.com>
2021-03-25 17:56:34 -0300

    camadas MVC

--- src/main/java/com/scheduleapp/ScheduleappApplication.java
+++ src/main/java/com/scheduleapp/ScheduleappApplication.java
@@ -1,4 +1,4 @@
-package com.scheduleapp.scheduleapp;
+package com.scheduleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/scheduleapp/controllers/indexController.java
+++ src/main/java/com/scheduleapp/controllers/indexController.java
@@ -1,4 +1,4 @@
-package com.scheduleapp.scheduleapp;
+package com.scheduleapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
